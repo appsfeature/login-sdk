a36887d0f947f12ca381cf99e3c4346b518caca0
helios <alphatestingdroid@gmail.com>
2021-12-23 17:08:44 +0530

    Update LoginDetail.java

--- login/src/main/java/com/appsfeature/login/LoginDetail.java
+++ login/src/main/java/com/appsfeature/login/LoginDetail.java
@@ -4,10 +4,26 @@ import android.content.Context;
 
 import com.appsfeature.login.interfaces.LoginType;
 import com.appsfeature.login.model.Profile;
+import com.appsfeature.login.util.LoginConstant;
 import com.appsfeature.login.util.LoginPrefUtil;
 import com.appsfeature.login.util.LoginUtil;
 
 public class LoginDetail {
+
+
+    public static boolean isRegComplete(Context context) {
+        return isRegComplete(context, LoginType.DEFAULT_USER);
+    }
+    public static boolean isRegComplete(Context context, @LoginType int loginType) {
+        return LoginPrefUtil.isRegComplete(context, loginType);
+    }
+
+    public static boolean isLoginComplete(Context context) {
+        return isLoginComplete(context, LoginType.DEFAULT_USER);
+    }
+    public static boolean isLoginComplete(Context context, @LoginType int loginType) {
+        return LoginPrefUtil.isLoginComplete(context, loginType);
+    }
 
     public static String getUserName(Context context) {
         return getUserName(context, LoginType.DEFAULT_USER);
